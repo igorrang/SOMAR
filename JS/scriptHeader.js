5b5f79497bb57c4db9a5596fd6bc653702eeaa11
igorrang <115603531+igorrang@users.noreply.github.com>
2023-05-03 23:22:38 -0300

    atualizacao 2

--- JS/scriptHeader.js
+++ JS/scriptHeader.js
@@ -4,6 +4,7 @@ let searchBox = document.querySelector('.searchBox');
 let navigation = document.querySelector('.navigation');
 let menuToggle = document.querySelector('.menuToggle');
 let header = document.querySelector('header');
+let user = document.querySelector('user')
 
 
 searchBtn.onclick=function(){
@@ -12,19 +13,25 @@ searchBtn.onclick=function(){
     searchBtn.classList.add('active');
     menuToggle.classList.add('hide');
     header.classList.toggle('open');
+    user.classList.add('hide')
 }
 closeBtn.onclick=function(){
     searchBox.classList.remove('active');
     searchBtn.classList.remove('active');
     closeBtn.classList.remove('active');
-    menuToggle.classList.remove('hide')
+    menuToggle.classList.remove('hide');
+    user.classList.remove('hide')
 }
 menuToggle.onclick = function(){
     header.classList.toggle('open');
     searchBox.classList.remove('active');
     searchBtn.classList.remove('active');
     closeBtn.classList.remove('active');
-    menuToggle.classList.remove('hide')
+    menuToggle.classList.remove('hide');
+    user.classList.remove('hide')
     
 }
 
+userRequire.onclick=function(){
+    user.classList.toggle('open');
+}
